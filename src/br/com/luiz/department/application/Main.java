9770b8324eabcbf9497d4a9e59ae59dbce5a8d56
Luiz Gomes <lgomes_kiau@hotmail.com>
2022-04-19 08:38:22 -0300

    Department entity class

---
+++ src/br/com/luiz/department/application/Main.java
@@ -0,0 +1,14 @@
+package br.com.luiz.department.application;
+
+import br.com.luiz.department.entitie.Department;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Department obj = new Department(1, "Books");
+		System.out.println(obj);
+
+	}
+
+}
