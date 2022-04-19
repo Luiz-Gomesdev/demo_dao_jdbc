87d73fff70bc9005e12ac7e60e5580d189ae8801
Luiz Gomes <lgomes_kiau@hotmail.com>
2022-04-19 08:56:09 -0300

    Seller entity class

--- src/br/com/luiz/department/application/Main.java
+++ src/br/com/luiz/department/application/Main.java
@@ -1,13 +1,19 @@
 package br.com.luiz.department.application;
 
+import java.util.Date;
+
 import br.com.luiz.department.entitie.Department;
+import br.com.luiz.department.entitie.Seller;
 
 public class Main {
 
 	public static void main(String[] args) {
 		
 		Department obj = new Department(1, "Books");
-		System.out.println(obj);
+		
+		Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, obj);
+		
+		System.out.println(seller);
 
 	}
 
