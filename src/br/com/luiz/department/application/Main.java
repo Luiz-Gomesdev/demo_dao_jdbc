f1e8e19b36bfca5a63549fc54b5081ea07f8e917
Luiz Gomes <lgomes_kiau@hotmail.com>
2022-04-20 08:50:46 -0300

    findByDepartment implementation

--- src/br/com/luiz/department/application/Main.java
+++ src/br/com/luiz/department/application/Main.java
@@ -1,7 +1,10 @@
 package br.com.luiz.department.application;
 
+import java.util.List;
+
 import br.com.luiz.department.dao.DaoFactory;
 import br.com.luiz.department.dao.SellerDao;
+import br.com.luiz.department.entitie.Department;
 import br.com.luiz.department.entitie.Seller;
 
 public class Main {
@@ -11,9 +14,16 @@ public class Main {
 		
 		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
+		System.out.println("=== TEST 1: seller findById ===");
 		Seller seller = sellerDao.findById(3);
-		
 		System.out.println(seller);
+		
+		System.out.println("\n=== TEST 2: seller findByDepartment ===");
+		Department department = new Department(2, null);
+		List<Seller> list = sellerDao.findByDepartment(department);
+		for (Seller obj : list) {
+			System.out.println(obj);
+		}
 
 	}
 
